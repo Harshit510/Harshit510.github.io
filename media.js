69be191976927705f287ddd589daa2c21b661208
Harshit Rana <rharshit510@github.com>
2020-08-03 21:44:12 +0530

    updated

---
+++ media.js
@@ -0,0 +1,20 @@
+    mdq {
+  background-color: tan;
+  color: black;
+}
+
+/* On screens that are 992px wide or less, the background color is blue */
+@media screen and (max-width: 992px) {
+  mdq {
+    background-color: blue;
+    color: white;
+  }
+}
+
+/* On screens that are 600px wide or less, the background color is olive */
+@media screen and (max-width: 600px) {
+  mdq {
+    background-color: olive;
+    color: white;
+  }
+}
